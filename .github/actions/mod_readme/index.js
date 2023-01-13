945a3367786139b7a92faf3f818235f15daf5a78
Vicent Esteve <esteve.ferre.vicent@gmail.com>
2023-01-13 12:06:18 +0100

    Subida Index.js y prueba y prueba Tests

--- .github/actions/mod_readme/index.js
+++ .github/actions/mod_readme/index.js
@@ -1,2 +1,28 @@
-const core = require('@actions/core');
-console.log("Hola soy la prubeaaa readme");
+const core = require("@actions/core");
+const path = require('path');
+const fs = require("fs");
+
+//Ruta Readme:
+const readmePath = path.resolve('../../../README.md');
+// const frase_positiva = core.getInput("frase_positiva");
+// const frase_negativa = core.getInput("frase_negativa");
+// const resultado_tests = core.getInput("resultado_tests");
+const URL= "https://api.memegen.link/images/img.png";
+
+const frase_positiva= "cryingfloor/casi.../error_tremndo";
+const frase_negativa= "vamoss!!/salio_bien_el_test";
+resultado_tests= "err";
+
+const MemeNegativo= URL.replace("img", frase_positiva);
+const MemePositivo= URL.replace("img", frase_negativa);
+
+var AddMeme = "![Meme Test](" + resultado_tests === "success" ? MemePositivo : MemeNegativo + ")"
+
+fs.readFile(readmePath, 'utf8', function(err, data) {
+    if (err) throw err;
+    var all_info = data = data + AddMeme;
+    fs.writeFile(readmePath, data, function(err) {
+        if (err) throw err;
+        console.log('Archivo actualizado');
+    });
+});
---
+++ README.md
@@ -0,0 +1,4 @@
+# Readme GitHub Action:
+### (Resultado Test Jest)
+
+![Meme Test](https://raw.githubusercontent.com/parzibyte/WaterPy/master/assets/ImagenV1.png)https://api.memegen.link/images/cryingfloor/casi.../error_tremndo.png)
