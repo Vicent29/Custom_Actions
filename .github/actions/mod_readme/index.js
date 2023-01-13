0d2ecd14b2e106b6c0a7494519a1efc0039e2ea7
Vicent Esteve <esteve.ferre.vicent@gmail.com>
2023-01-13 12:59:25 +0100

    Test Positive v1

--- .github/actions/mod_readme/index.js
+++ .github/actions/mod_readme/index.js
@@ -2,27 +2,27 @@ const core = require("@actions/core");
 const path = require('path');
 const fs = require("fs");
 
-//Ruta Readme:
+//Declaration of variables
 const readmePath = path.resolve('../../../README.md');
-// const frase_positiva = core.getInput("frase_positiva");
-// const frase_negativa = core.getInput("frase_negativa");
-// const resultado_tests = core.getInput("resultado_tests");
+const frase_positiva = core.getInput("frase_positiva");
+const frase_negativa = core.getInput("frase_negativa");
+const resultado_tests = core.getInput("resultado_tests");
 const URL= "https://api.memegen.link/images/img.png";
-
-const frase_positiva= "cryingfloor/casi.../error_tremndo";
-const frase_negativa= "vamoss!!/salio_bien_el_test";
-resultado_tests= "err";
 
 const MemeNegativo= URL.replace("img", frase_positiva);
 const MemePositivo= URL.replace("img", frase_negativa);
 
+//Add line img meme
 var AddMeme = "![Meme Test](" + resultado_tests === "success" ? MemePositivo : MemeNegativo + ")"
 
+//Change Readme
 fs.readFile(readmePath, 'utf8', function(err, data) {
     if (err) throw err;
-    var all_info = data = data + AddMeme;
+    data += AddMeme;
     fs.writeFile(readmePath, data, function(err) {
         if (err) throw err;
-        console.log('Archivo actualizado');
+        console.log("Readme Modificado correctamente");
+        core.setOutput('msg', "Readme Modificado correctamente");
+        process.exit(0)
     });
 });
